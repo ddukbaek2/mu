a66e0af649bc4029db907edbbf4109775f3ff892
ddukbaek2 <ddukbaek2@gmail.com>
2024-09-28 19:15:19 +0900

    작업.

--- core/core.c
+++ core/core.c
@@ -1,15 +1,4 @@
 ﻿//------------------------------------------------------------------------------
 // 헤더 파일 목록.
 //------------------------------------------------------------------------------
-#pragma once
-#include <stdint.h>
-
-
-typedef struct __mulang_object__ {
-	uint64_t unique_id;
-} mulang_object;
-
-
-typedef struct __mulang_object2__ {
-	uint64_t unique_id;
-} mulang_object2;
+#include "core.h"
---
+++ core/list.h
@@ -0,0 +1,53 @@
+//------------------------------------------------------------------------------
+// 헤더 파일 목록.
+//------------------------------------------------------------------------------
+#pragma once
+#include <stdint.h>
+#include "constant.h"
+
+
+//------------------------------------------------------------------------------
+// 노드.
+//------------------------------------------------------------------------------
+struct __node__ {
+	struct __node__* previous;
+	struct __node__* next;
+	unknown_instance data;
+};
+
+
+//------------------------------------------------------------------------------
+// 형식 재정의.
+//------------------------------------------------------------------------------
+typedef struct __node__ node;
+typedef struct __node__* node_instance;
+typedef struct __node__** node_instance_ptr;
+
+
+//------------------------------------------------------------------------------
+// 리스트.
+//------------------------------------------------------------------------------
+struct __list__ {
+	node_instance head;
+	node_instance tail;
+};
+
+
+//------------------------------------------------------------------------------
+// 형식 재정의.
+//------------------------------------------------------------------------------
+typedef struct __list__ list;
+typedef struct __list__* list_instance;
+typedef struct __list__** list_instance_ptr;
+
+
+//------------------------------------------------------------------------------
+// 리스트 함수 목록.
+//------------------------------------------------------------------------------
+list_instance list_create();
+void list_destroy(list_instance_ptr instance_ptr);
+void list_add(list_instance instance, unknown_instance value);
+void list_remove(list_instance instance, unknown_instance value);
+void list_set_value(list_instance instance, uint64_t index, unknown_instance value);
+unknown_instance list_get_value(list_instance instance, uint64_t index);
+int64_t list_index_of(list_instance instance, unknown_instance element);
